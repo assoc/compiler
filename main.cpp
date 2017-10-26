513d00915be327e371a55b915c59805ca19c493a
assoc <assoc42@gmail.com>
2017-10-26 07:54:43 +0300

    More comments

--- main.cpp
+++ main.cpp
@@ -1,18 +1,31 @@
 #include <stack>
-#include <vector>
 #include <string>
+#include <vector>
 
 using std::stack;
 using std::string;
 using std::vector;
 
-enum lclass {UNDEF, COMMA, NUMBER, PLUS, MINUS, MULT, DIVIDE, POWER, L_BR, R_BR, NEWL, UNARY};
-char types[][6] = {"UNDEF", "COMMA", "NUMB\0", "PLUS\0", "MINUS", "MULT", "DIVID", "POWER", "L_BR\0", "R_BR\0", "NEWL\0", "UNARY"};
+enum lexem_class {
+	UNDEF,	// undefined symbol
+	COMMA,	// comma (dot)
+	NUMBER,	// digit/number
+	PLUS,	// addition
+	MINUS,	// subtraction
+	MULT,	// multiplication
+	DIVIDE,	// division
+	POWER,	// power (exponential)
+	L_BR,	// left bracket
+	R_BR,	// right bracket
+	NEWL,	// new line or EOF
+	UNARY	// unary minus
+};
 
-typedef struct token {
-	lclass code;
-	long long number;
-	unsigned line;
+typedef struct token
+{
+	lexem_class code;
+	long long	number;
+	unsigned	line;
 
 	bool is_number()
 	{ return code == NUMBER; }
@@ -22,29 +35,28 @@ typedef struct token {
 	{ return code == POWER; }
 	bool is_binary_operator(){ return is_left_operator() || is_right_operator(); }
 	bool is_unary_operator()
-	{ return code == MINUS; }
+	{ return code == MINUS || code == UNARY; }
 	bool is_operator()
 	{ return is_binary_operator() || is_unary_operator(); }
 } token;
 
 
-stack<token*>			operators;
-vector<token*>			output;
+
 
 
 namespace lexer
 {
 	bool is_alpha(char c)
 	{
-		return (c >= 'a' && c <= 'z') ? 1 : 0;
+		return (c >= 'a' && c <= 'z');
 	}
 	bool is_digit(char c)
 	{
-		return (c >= '0' && c <= '9') ? 1 : 0;
+		return (c >= '0' && c <= '9');
 	}
 	bool is_space(char c)
 	{
-		return (c == ' ' || c == '\t') ? 1 : 0;
+		return (c == ' ' || c == '\t');
 	}
 
 	vector<token> parse(string& input)
@@ -55,34 +67,42 @@ namespace lexer
 
 		for (auto& it = input.begin(); it != input.end(); ++it)
 		{
+			// skip all spaces
 			if (is_space(*it))
 				continue;
 			
 			if (is_digit(*it))
 			{
+				// numbers
+
 				number_as_text.clear();
 				number_as_text += *it;
 
+				// add digits until something else is found
 				for (++it; it != input.end() && is_digit(*it); ++it)
 					number_as_text += *it;
 
-				// return back to non-digit symbol
-				--it;
-
 				try
 				{
+					// convert string to actual number and add it to the list
 					tokens.push_back({ NUMBER, std::stoll(number_as_text.c_str()), current_line });
 				}
 				catch (std::out_of_range&)
 				{
+					// if number is too big or too small mark it as undefined
 					tokens.push_back({ UNDEF, 0, current_line });
 					printf("\n (%d) lexer: out of range number '%s'", current_line, number_as_text.c_str());
 				}
+
+				// return back to last non-digit symbol
+				--it;
 
 				continue;
 			}
 			else
 			{
+				// operators
+
 				switch (*it)
 				{
 				case '+':	tokens.push_back({ PLUS,	0, current_line }); break;
@@ -106,6 +126,7 @@ namespace lexer
 			}
 		}
 
+		// last symbol
 		tokens.push_back({ NEWL, 0, current_line });
 		return tokens;
 	}
@@ -113,10 +134,8 @@ namespace lexer
 
 namespace parser
 {
-
 	vector<token>::iterator current_token;
 	vector<token>::iterator end_token;
-	unsigned				errors;
 
 	void consume()
 	{
@@ -134,7 +153,7 @@ namespace parser
 		throw std::exception("Parsing error");
 	}
 
-	void expect(lclass tok)
+	void expect(lexem_class tok)
 	{
 		if (next().code == tok)
 			consume();
@@ -142,8 +161,8 @@ namespace parser
 			error();
 	}
 
-	void E();
-	void P()
+	void expression();
+	void term()
 	{
 		if (next().is_number())
 		{
@@ -152,14 +171,14 @@ namespace parser
 		else if (next().code == L_BR)
 		{
 			consume();
-			E();
+			expression();
 			expect(R_BR);
 		}
 		else if (next().is_unary_operator())
 		{
 			next().code = UNARY;
 			consume();
-			P();
+			term();
 		}
 		else
 		{
@@ -167,14 +186,22 @@ namespace parser
 		}
 	}
 
-	void E()
+	/*
+		digit -> "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+		number -> digit | number {digit}
+		b_op -> "+" | "-" | "*" | "/" | "^"
+		expression -> term | term b_op term
+		term -> number | "(" expression ")" | "-" term
+	*/
+
+	void expression()
 	{
-		P();
+		term();
 
 		while (next().is_binary_operator())
 		{
 			consume();
-			P();
+			term();
 		}
 	}
 
@@ -183,13 +210,12 @@ namespace parser
 		if (tokens.empty())
 			return;
 
-		errors			= 0;
 		current_token	= tokens.begin();
 		end_token		= tokens.end();
 
 		do 
 		{
-			E();
+			expression();
 			expect(NEWL);
 		} while (next().code != NEWL);
 	}
@@ -197,6 +223,9 @@ namespace parser
 
 namespace yard
 {
+	stack<token*>			operators;
+	vector<token*>			output;
+
 	vector<token>::iterator current_token;
 	vector<token>::iterator end_token;
 	unsigned				errors;
@@ -207,7 +236,7 @@ namespace yard
 			++current_token;
 	}
 
-	bool is_higher(lclass a, lclass b)
+	bool is_higher(lexem_class a, lexem_class b)
 	{
 		char priority = 0;
 
@@ -238,38 +267,13 @@ namespace yard
 			next_token();
 	}
 
-	/*
-	while there are tokens to be read:
-	read a token.
-	if the token is a number, then push it to the output queue.
-	if the token is an operator, then:
-	while there is an operator at the top of the operator stack with
-	greater than or equal to precedence and the operator is left associative:
-	pop operators from the operator stack, onto the output queue.
-	push the read operator onto the operator stack.
-	if the token is a left bracket (i.e. "("), then:
-	push it onto the operator stack.
-	if the token is a right bracket (i.e. ")"), then:
-	while the operator at the top of the operator stack is not a left bracket:
-	pop operators from the operator stack onto the output queue.
-	pop the left bracket from the stack.
-	< if the stack runs out without finding a left bracket, then there are
-	mismatched parentheses. >
-	if there are no more tokens to read:
-	while there are still operator tokens on the stack:
-	< if the operator token on the top of the stack is a bracket, then
-	there are mismatched parentheses. >
-	pop the operator onto the output queue.
-	exit.
-	*/
-
 	void apply_operation()
 	{
 		token* operation = operators.top();
 		if (operation->is_binary_operator())
 		{
 			if (output.size() < 2)
-				__debugbreak();
+				throw std::exception("Not enough operands for binary operator");
 
 			token* op2 = output.back();
 			output.pop_back();
@@ -281,7 +285,12 @@ namespace yard
 			case PLUS:		op1->number += op2->number;	break;
 			case MINUS:		op1->number -= op2->number;	break;
 			case MULT:		op1->number *= op2->number;	break;
-			case DIVIDE:	op1->number /= op2->number;	break; // TODO: /0
+			case DIVIDE:
+				if (op2->number == 0)
+					throw std::exception("Division by 0");
+				op1->number /= op2->number;
+				break;
+
 			case POWER:		op1->number = pow(op1->number, op2->number); break;
 			}
 
@@ -290,7 +299,7 @@ namespace yard
 		else
 		{
 			if (output.size() < 1)
-				__debugbreak();
+				throw std::exception("Not enough operands for unary operator");
 
 			output.back()->number = -output.back()->number;
 		}
@@ -298,34 +307,46 @@ namespace yard
 
 	void shunting_yard()
 	{
+		// while there are tokens to be read...
 		while (current_token->code != NEWL /*&& it->code != END*/)
 		{
+			// if the token is a number, then push it to the output queue.
 			if (current_token->is_number())
 			{
 				output.push_back(&(*current_token));
 			}
+			// if the token is an operator, then:
 			else if (current_token->is_operator())
 			{
-				if (!operators.empty())
+				// while there is an operator at the top of the operator stack
+				// with greater than or equal to precedence and the operator is left associative:
+				while (		!operators.empty()
+						&& 	!is_higher(current_token->code, operators.top()->code)
+						&& 	current_token->is_left_operator())
 				{
-					while (!operators.empty() && !is_higher(current_token->code, operators.top()->code) && current_token->is_left_operator())
-					{
-						apply_operation(); // output.push_back(operators.top());
-						operators.pop();
-					}
+					// pop operators from the operator stack, onto the output queue.
+					apply_operation();
+					operators.pop();
 				}
 
+				// push the read operator onto the operator stack.
 				operators.push(&(*current_token));
 			}
+			// if the token is a left bracket (i.e. "("), then:
 			else if (current_token->code == L_BR)
 			{
+				// push it onto the operator stack.
 				operators.push(&(*current_token));
 			}
+			// if the token is a right bracket (i.e. ")"), then:
 			else if (current_token->code == R_BR)
 			{
 				bool bracket_pair_found = false;
+
+				// while the operator at the top of the operator stack is not a left bracket:
 				while (!operators.empty() && !bracket_pair_found)
 				{
+					// pop operators from the operator stack onto the output queue.
 					if (operators.top()->code != L_BR)
 					{
 						apply_operation();
@@ -335,9 +356,11 @@ namespace yard
 						bracket_pair_found = true;
 					}
 
+					// pop the left bracket from the stack.
 					operators.pop();
 				}
 
+				// (if the stack runs out without finding a left bracket, then there are mismatched parentheses.)
 				if (!bracket_pair_found)
 				{
 					shunting_yard_error();
@@ -345,13 +368,19 @@ namespace yard
 				}
 			}
 
+			// ... read a token
 			next_token();
 		}
 
+		// if there are no more tokens to read:
+
+		// while there are still operator tokens on the stack:
 		while (!operators.empty())
 		{
+			// ( if the operator token on the top of the stack is a bracket, then there are mismatched parentheses. )
 			if (operators.top()->code != L_BR)
 			{
+				// pop the operator onto the output queue.
 				apply_operation();
 				operators.pop();
 			}
@@ -362,16 +391,16 @@ namespace yard
 			}
 		}
 
-		// Output as string
-		for (auto i = output.begin(); i != output.end(); ++i)
+		// output result of the calculation
+		for (auto& i : output)
 		{
-			if ((*i)->code == NUMBER) { printf("%d", (*i)->number); }
-			else if ((*i)->code == PLUS) { printf("+ "); }
-			else if ((*i)->code == MINUS) { printf("- "); }
-			else if ((*i)->code == MULT) { printf("* "); }
-			else if ((*i)->code == DIVIDE) { printf("/ "); }
-			else if ((*i)->code == POWER) { printf("^ "); }
-			else if ((*i)->code == UNARY) { printf("~ "); }
+			if (i->code == NUMBER) { printf("%d", i->number); }
+			else if (i->code == PLUS) { printf("+ "); }
+			else if (i->code == MINUS) { printf("- "); }
+			else if (i->code == MULT) { printf("* "); }
+			else if (i->code == DIVIDE) { printf("/ "); }
+			else if (i->code == POWER) { printf("^ "); }
+			else if (i->code == UNARY) { printf("~ "); }
 		}
 	}
 
@@ -395,9 +424,10 @@ namespace yard
 
 int main(int argc, char* argv[])
 {
-	char buf_input[] =	"-3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3"	"\n"
+	char buf_input[] =	"-3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3""\n"
 						"2 + 2 * 2"						"\n"
-						"100 * ( 2 + 12 ) / 14"			"\n"; // TODO: parser failed when 14 deleted
+						"100 * ( 2 + 12 ) / 14"			"\n"
+						"- (14/2+3) * -( -3 + 8 / 2 )"	"\n";
 
 	vector<token> tokens = lexer::parse(string(buf_input));
 
